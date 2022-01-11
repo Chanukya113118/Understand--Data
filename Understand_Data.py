bb7c0419fe951bde2872a02e78c3620345914b95
Chanukya22 <93651194+Chanukya22@users.noreply.github.com>
2022-01-11 15:32:42 +0530

    Update Understand_Data.py

--- Understand_Data.py
+++ Understand_Data.py
@@ -3,13 +3,16 @@ import pandas_profiling as pp
 import streamlit as st
 import streamlit_pandas_profiling as spp
 data=pd.DataFrame()
+@st.cache
 def fun(data):
     profile=pp.ProfileReport(data)
     st.write(data)
     spp.st_profile_report(profile)
 st.write('Understand your Data for better decision making.')
-file=st.file_uploader('Choose the file')
-if file is not None:
-    data=pd.read_csv(file)
-    fun(data)
-        
+def m():
+    file=st.file_uploader('Choose the file')
+    if file is not None:
+        data=pd.read_csv(file)
+        fun(data)
+if __name__=='__main__':
+    m()
