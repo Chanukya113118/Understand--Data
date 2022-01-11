a662486d97ae0ab0c66da87637b313fb40b9ab05
Chanukya22 <93651194+Chanukya22@users.noreply.github.com>
2022-01-11 14:33:49 +0530

    Add files via upload

---
+++ Understand_Data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pandas_profiling as pp
+import streamlit as st
+import streamlit_pandas_profiling as spp
+data=pd.DataFrame()
+def fun(data):
+    profile=pp.ProfileReport(data)
+    st.write(data)
+    spp.st_profile_report(profile)
+st.write('Understand your Data for better decision making.')
+file=st.file_uploader('Choose the file')
+if file is not None:
+    data=pd.read_csv(file)
+    fun(data)
+        
