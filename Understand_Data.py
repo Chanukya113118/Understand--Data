d74b957fc6baf72d133a96da89a510e4b1ff7929
Chanukya22 <93651194+Chanukya22@users.noreply.github.com>
2022-01-29 13:29:20 +0530

    Update Understand_Data.py
    Try except 5o read CSV file only.

--- Understand_Data.py
+++ Understand_Data.py
@@ -9,9 +9,12 @@ def fun(data):
     spp.st_profile_report(profile)
 st.write('Understand your Data for better decision making.')
 def m():
+  try:
     file=st.file_uploader('Choose the file')
     if file is not None:
         data=pd.read_csv(file)
         fun(data)
+   except:
+       print("try again")
 if __name__=='__main__':
     m()
